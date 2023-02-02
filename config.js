f28568804045381819a4207c7807d1e6037741b7
Santuxyz <124232319+Santuxyz@users.noreply.github.com>
2023-02-02 11:12:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,46 +1,127 @@
 import { BsFacebook, BsYoutube } from "react-icons/bs";
+
 import { CgCrown } from "react-icons/cg";
+
 import { FaDiscord } from "react-icons/fa";
+
 import { MdAddModerator } from "react-icons/md";
-import { SgMusic } from "react-icons/si";
+
+import { SiProbot } from "react-icons/si";
+
 import { TbMessage2Code } from "react-icons/tb";
+
 const config = {
-",
+
+  name: "ProBot",
+
   //BotName
+
   inviteLink:
-    "https://discord.com/api/oauth2/authorize?client_id=1056105404456771654&permissions=8&scope=bot",
+
+    "https://discord.com/oauth2/authorize?client_id=879602399067660338&permissions=8&scope=bot&redirect_uri=https://docs.markos.ga&response_type=code",
+
   //BotInvite Link
+
   Features: [
+
     {
-      name: "MUSIC",
+
+      name: "Moderation",
+
       description:
-        "Feature rich and high quality music bot with great features for your server...",
+
+        "Moderation is the process of eliminating or lessening extremes.",
+
       logo: <MdAddModerator className="h-10 w-10" />,
+
     },
 
+    {
+
+      name: "99% Uptime",
+
+      description:
+
+        "Uptime is a measure of system reliability, expressed as the percentage of time a machine, typically a computer.",
+
+      logo: <SiProbot className="h-10 w-10" />,
+
+    },
+
+    {
+
+      name: "Automation",
+
+      description:
+
+        "Automation describes a wide range of technologies that reduce  human intervention in processes.",
+
+      logo: <TbMessage2Code className="h-10 w-10" />,
+
+    },
+
+    {
+
+      name: "Welcomer",
+
+      description:
+
+        "Welcomer is the person who greet the new person in an unique and great way",
+
+      logo: <CgCrown className="h-10 w-10" />,
+
+    },
+
+    // You can added more features if need add
+
+  ],
+
   //Bot Features
-  serverCount: 30, //-  - Your
-  memberCount: 102, //--   - Bot
-  executedCommand: 7000, //--  - Stats
-  availableCommand: 100, //---   - Here
+
+  serverCount: 300, //-  - Your
+
+  memberCount: 700, //--   - Bot
+
+  executedCommand: 27000, //--  - Stats
+
+  availableCommand: 400, //---   - Here
+
   //Do not change if you don't know about them
+
   faceBookLogo: (
+
     <BsFacebook className="h-10 w-10 text-[#4267B2] hover:scale-125 easeIn cursor-pointer" />
+
   ),
+
   discordLogo: (
+
     <FaDiscord className="h-10 w-10 text-[#738adb] hover:scale-125 easeIn cursor-pointer" />
+
   ),
+
   youTubeLogo: (
+
     <BsYoutube className="h-10 w-10 text-[#FF0000] hover:scale-125 easeIn cursor-pointer" />
+
   ),
+
   //----------------------------------------------------------------
+
   // Your social handle links
-  githubLink: "",
-  twitterLink: "",
-  mediumLink: "",
-  facebookLink: "",
-  discordLink: "https://discord.gg/SMbtGmYhtz",
-  youtubeLink: "",
+
+  githubLink: "https://github.com/mohitmimani",
+
+  twitterLink: "https://twitter.com",
+
+  mediumLink: "https://medium.com/",
+
+  facebookLink: "https://facebook.com",
+
+  discordLink: "https://discord.com",
+
+  youtubeLink: "https://www.youtube.com/channel/UCfhRHeex9lU0ZAVJot6Z2ZQ",
+
 };
 
 export default config;
